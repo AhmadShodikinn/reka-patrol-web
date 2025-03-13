ee84109be4b5a514bad5fe5652879d8008cb184d
AhmadShodikinn <dikiahmad05@gmail.com>
2025-03-13 14:51:11 +0700

    initalize project

---
+++ database/migrations/2024_10_21_091446_inspection_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inspection', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('worker_id')->constrained('users');
+            $table->foreignId('pic_id')->constrained('users');
+            $table->string('activity');
+            $table->string('target');
+            $table->string('category');
+            $table->string('image_path_before');
+            $table->string('status');
+            $table->string('image_path_after');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
