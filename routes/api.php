83168c2d163f3f6d8db06b3c8448c4649d2282dd
I <91296724+i9191@users.noreply.github.com>
2025-03-17 21:36:17 +0700

    add API authentication controller and request validation

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\ApiAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['as' => 'api.'], function () {
+    Route::post('login', [ApiAuthController::class, 'login'])->name('login');
+});
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
