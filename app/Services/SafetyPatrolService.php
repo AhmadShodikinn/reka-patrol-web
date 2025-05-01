b5eacd3d069a2fe6cf487db2837cbbbba39b036e
I <91296724+i9191@users.noreply.github.com>
2025-05-01 18:59:26 +0700

    feat: add multiple image request validation and services for safety patrols

--- app/Services/SafetyPatrolService.php
+++ app/Services/SafetyPatrolService.php
@@ -4,13 +4,14 @@ namespace App\Services;
 
 use App\Models\SafetyPatrol;
 use App\Traits\HandleImage;
+use Illuminate\Support\Arr;
 
 class SafetyPatrolService
 {
     use HandleImage;
 
     public function handleSafetyPatrolImageUpload(array $data, SafetyPatrol $safetyPatrol = null): array {
-        $data = $this->handleImageUpload(data: $data, keyOrModel: $safetyPatrol, imageField: 'findings_path', customPath: 'safety-patrol/findings');
+        $data = $this->handleArrayImageUpload(data: $data, keyOrModel: $safetyPatrol, imageField: 'finding_paths', customPath: 'safety-patrol/findings');
         $data = $this->handleImageUpload(data: $data, keyOrModel: $safetyPatrol, imageField: 'action_path', customPath: 'safety-patrol/actions');
         return $data;
     }
@@ -20,7 +21,17 @@ class SafetyPatrolService
         if (!isset($data['worker_id'])) {
             $data['worker_id'] = auth()->id();
         }
-        return SafetyPatrol::create($data);
+        $safetyPatrol = SafetyPatrol::create(
+            Arr::except($data, ['finding_paths'])
+        );
+        $safetyPatrol->findings()->createMany(
+            collect($data['finding_paths'])
+                ->map(fn($path, $key) => [
+                    'image_path' => $path,
+                ])
+                ->toArray()
+        );
+        return $safetyPatrol;
     }
 
     public function update(SafetyPatrol $safetyPatrol, array $data): ?SafetyPatrol {
