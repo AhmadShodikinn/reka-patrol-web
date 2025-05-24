8f19ebf4537128c4de2666e3320d40c7a8d01824
I <91296724+i9191@users.noreply.github.com>
2025-05-24 21:32:25 +0700

    add auth relations loader🗿

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\ApiLoginRequest;
+use App\Http\Resources\UserResource;
 
 class ApiAuthController extends Controller
 {
     public function login(ApiLoginRequest $request) {
         if (auth()->attempt($request->only('nip', 'password'))) {
             return response()->json([
-                'user' => auth()->user(),
+                'user' => UserResource::make(auth()->user()->load($request['relations'] ?? [])),
                 'token' => auth()->user()->createToken('token')->plainTextToken,
             ]);
         }
